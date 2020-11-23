4c23b6350998b83d5b6626568327863a1aa22206
janfredrikaasmundtveit <42673867+janfredrikaasmundtveit@users.noreply.github.com>
2020-11-23 16:32:39 +0100

    final

--- project4/nopar.cpp
+++ project4/nopar.cpp
@@ -2,7 +2,6 @@
 #include <iostream>
 #include <cmath>
 #include <iomanip>
-#include <mpi.h>
 #include <ctime>
 #include <fstream>
 #include <string>
@@ -15,13 +14,11 @@ using namespace std;
 ofstream ofile;
 
 
-
-void mcsampling(int MCC, double temp,int NSpin, mat &lattice,double *Energy,double *MagneticMoment,double *TotE,double *TotE2,double *TotM,double *TotM2,double *TotabsM, int *accepted){
+void mcsampling(int MCC, double temp,int NSpin, mat &lattice,double *Energy,double *MagneticMoment,double *TotE,double *TotE2,double *TotM,double *TotM2,double *TotabsM, double *accepted){
   std::random_device rd;
   std::mt19937_64 gen(rd());
   // Set up the uniform distribution for x \in [[0, 1]
   std::uniform_real_distribution<double> RandomNumberGenerator(0.0,1.0);
-
   int totspin=NSpin*NSpin;
   vec EnergyDifference=zeros<vec>(17); 
 
@@ -36,31 +33,38 @@ void mcsampling(int MCC, double temp,int NSpin, mat &lattice,double *Energy,doub
         int ix = (int) (RandomNumberGenerator(gen)*(NSpin));
         int iy = (int) (RandomNumberGenerator(gen)*(NSpin));
     
-        int deltaE=2*lattice(ix,iy)*(lattice(ix,(iy+1)%NSpin)+lattice(ix,(iy+NSpin-1)%NSpin)+lattice((ix+1)%NSpin,iy)+lattice((ix+NSpin-1)%NSpin,iy));
+        int deltaE=2*lattice(ix,iy)*(lattice(ix,(iy+1)%NSpin)+lattice(ix,(iy+(NSpin)-1)%NSpin)+lattice((ix+1)%NSpin,iy)+lattice((ix+(NSpin)-1)%NSpin,iy));
            
         if( RandomNumberGenerator(gen) <= EnergyDifference(deltaE+8)){
             lattice(ix,iy) *=-1.0;
             *Energy +=((double)(deltaE));
             *MagneticMoment+=2.0*lattice(ix,iy); 
-            accepted++;
-          } 
-          
-       
+           // *accepted+=1.0;
+          }
+         
+       }
+
      
         if(i>1000){
-   
-        *TotE += *Energy; *TotE2 += (*Energy)*(*Energy); *TotM += *MagneticMoment; *TotM2 +=(*MagneticMoment)*(*MagneticMoment); *TotabsM += fabs(*MagneticMoment);
+          *TotE += *Energy; *TotE2 += (*Energy)*(*Energy); *TotM += *MagneticMoment; *TotM2 +=(*MagneticMoment)*(*MagneticMoment); *TotabsM += fabs(*MagneticMoment);
          }
-    
+
+
+            /*
+            ofile << setw(15) << setprecision(8) << i;
+            ofile << setw(15) << setprecision(8) << *Energy/(NSpin*NSpin); 
+            ofile << setw(15) << setprecision(8) << *MagneticMoment/(NSpin*NSpin)<< "\n";//for finding where equlibrium
+            */
+
 
   }
+  
 }
 
-}
-
-void output(double temp, int NSpin, int MCC,double TotE[],int NProcesses){
+void output(double temp, int NSpin, int MCC,double TotE[]){
 //lattice size, cycles, temprature, other
- double norm = 1.0/((double) (MCC-1000)*NProcesses);  // divided by  number of cycles
+ double norm = 1.0/((double) (MCC-1000));  // divided by  number of cycles
+ double n=1.0/((double)(MCC)); 
    double AllSpins = 1.0/((double) NSpin*NSpin);
    double E_ExpectationValues = TotE[0]*norm; 
   double E2_ExpectationValues = TotE[1]*norm; 
@@ -80,7 +84,6 @@ void output(double temp, int NSpin, int MCC,double TotE[],int NProcesses){
 
 
 
-
 }
 
 
@@ -89,48 +92,46 @@ int main (int argc, char* argv[])
 { string filename;
   int NSpin, MCC, mc;
   double ITemp, FTemp, TempStep;
-  int NProcesses, RankProcess;
+ 
 
 
 
-//   MPI initializations
-MPI_Init (&argc, &argv);
-  MPI_Comm_size (MPI_COMM_WORLD, &NProcesses);
-  MPI_Comm_rank (MPI_COMM_WORLD, &RankProcess);
-// set up in master op
-
- if (RankProcess == 0 && argc < 3) {
+ if (argc < 3) {
     cout << "Bad Usage: " << argv[0] << 
       " read output file, Number of spins and MC cycles" << endl;
     exit(1);
   } 
-  if ((RankProcess == 0) && (argc > 1)) {
+  if (argc > 1) {
     filename=argv[1];
     NSpin = atoi(argv[2]);
     mc = atoi(argv[3]);  //mccycles  
     MCC=pow(10,mc);
-    ITemp =2.0;
+    ITemp =2.2;
     FTemp = 2.3;
-    TempStep = 1;
+    TempStep = 0.01;
   }
 
-
-
-  if (RankProcess == 0) {
     string fileout = filename;
     ofile.open(fileout);
-  }
- 
-  MPI_Bcast (&MCC, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Bcast (&NSpin, 1, MPI_INT, 0, MPI_COMM_WORLD);
-  MPI_Bcast (&ITemp, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-  MPI_Bcast (&FTemp, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-  MPI_Bcast (&TempStep, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+  
+
 
   double  TimeStart, TimeEnd, TotalTime;
   //TimeStart = MPI_Wtime();
- // setting groundstate=0
- mat lattice=ones<mat>(NSpin,NSpin); 
+     std::random_device rd;
+  std::mt19937_64 gen(rd());
+  // Set up the uniform distribution for x \in [[0, 1]
+  std::uniform_real_distribution<double> RandomNumberGenerator(0.0,1.0);
+// for(int m=1000; m<MCC;m=10*m){ //loop for comparing accepeted configurations 
+  mat lattice=ones<mat>(NSpin,NSpin); 
+  for(int i=0;i<NSpin;i++){ //randomize lattice comment out to use groundstate
+    for(int j=0;j<NSpin;j++){
+        if( RandomNumberGenerator(gen) <=0.5){
+          lattice(i,j)=-1;
+        }
+      }
+
+ }
   double Energy= NSpin*NSpin*(-2); //E 
   double MagneticMoment=NSpin*NSpin; //M
 	 
@@ -140,35 +141,36 @@ MPI_Init (&argc, &argv);
     double TotM = 0.0;
     double TotM2 = 0.0;
     double  TotabsM = 0.0;
-    int accepted=0;
-     mcsampling(MCC,temp,NSpin,lattice,&Energy,&MagneticMoment,&TotE,&TotE2,&TotM,&TotM2,&TotabsM,&accepted);
+    double accepted=0.0; //number of accepted configurations 
+    //double temp=1.0; //fix temprature coment out if looping over tempratures
+   
+   /*    mcsampling(m,temp,NSpin,lattice,&Energy,&MagneticMoment,&TotE,&TotE2,&TotM,&TotM2,&TotabsM,&accepted);//use if looping over number of mccycles for comparing accepeted configurations comment out otherwise
+
+       ofile << setw(15) << setprecision(8) << m;
+       ofile << setw(15) << setprecision(8) << (accepted)/((double)(m*NSpin*NSpin)) << "\n";*/
+    mcsampling(MCC,temp,NSpin,lattice,&Energy,&MagneticMoment,&TotE,&TotE2,&TotM,&TotM2,&TotabsM,&accepted);//use if not looping over number of mccycles
   		
-    double LocE[5]; //E,E^2,M,M^2,|M|
+  	double LocE[5]; //E,E^2,M,M^2,|M|
     LocE[0]=TotE;
     LocE[1]=TotE2;
     LocE[2]=TotM;
     LocE[3]=TotM2;
     LocE[4]=TotabsM;
-    double Tot[]={0.0,0.0,0.0,0.0,0.0};  
 
-	  
 
-   
-  	MPI_Reduce(&LocE, &Tot, 5, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
-
-  	if ( RankProcess == 0){ 
-  		output(temp,NSpin,MCC,Tot,NProcesses);	
+	 
+  		output(temp,NSpin,MCC,LocE);	
      
-		}
 
- }
+
+  }
 
   
-    if(RankProcess == 0){ 
-     ofile.close(); } // close output file
+ 
+     ofile.close();  // close output file
   
   //  End MPI
-MPI_Finalize ();
+
 
 return 0;
 }
